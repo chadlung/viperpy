d5fd5132c694128824a9f21d618a155eefd24549
Chad Lung <chad.lung@gmail.com>
2015-02-25 22:09:12 -0600

    Add additional unit tests for TokenRequest

--- tests/util/token_request_test.py
+++ tests/util/token_request_test.py
@@ -1,4 +1,5 @@
 # Standard lib imports
+import httplib
 import unittest
 
 # Third party imports
@@ -8,6 +9,7 @@ from mock import patch
 
 # Project level imports
 from viperpy.util.token_request import TokenRequest
+from viperpy.util.exceptions import ViperpyException
 
 
 def suite():
@@ -20,7 +22,8 @@ class WhenTestingTokenRequest(unittest.TestCase):
 
     def setUp(self):
         self.token_file_contents = '123TOKEN'
-        self.conf = MagicMock()
+        self.response = MagicMock()
+        self.session_get = 'viperpy.util.token_request.requests.Session.get'
 
         self.token_request = TokenRequest(username='someone',
                                           password='password',
@@ -42,6 +45,24 @@ class WhenTestingTokenRequest(unittest.TestCase):
     def test_should_not_get_existing_token(self):
         with patch('os.path.isfile', return_value=False):
             self.assertEqual(self.token_request._get_existing_token(), None)
+
+    def test_get_new_token_should_throw_viperpyexception_500(self):
+        self.response.status_code = httplib.INTERNAL_SERVER_ERROR
+        self.requests = MagicMock(return_value=self.response)
+        self.requests.get.side_effect = [self.response]
+
+        with patch(self.session_get, self.requests):
+            with self.assertRaises(ViperpyException):
+                self.token_request.get_new_token()
+
+    def test_get_new_token_should_throw_viperpyexception_401(self):
+        self.response.status_code = httplib.UNAUTHORIZED
+        self.requests = MagicMock(return_value=self.response)
+        self.requests.get.side_effect = [self.response]
+
+        with patch(self.session_get, self.requests):
+            with self.assertRaises(ViperpyException):
+                self.token_request.get_new_token()
 
 if __name__ == '__main__':
     unittest.main()
