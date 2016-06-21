c5582b2b3653080e7b78c5439b901b07103af3e4
Philipp Schmitt <philipp@schmitt.co>
2016-06-21 15:38:17 +0200

    Add example JSON response

--- viperpy/api/license.py
+++ viperpy/api/license.py
@@ -12,7 +12,22 @@ class License():
 
         Example JSON result from the API:
 
-        {"user": "root"}
+        {
+          'license_feature': [{
+            'expired_ind': False,
+            'issued_date': '06/21/2016',
+            'license_id_indicator': 'U',
+            'licensed_ind': True,
+            'model': 'ViPR_Controller',
+            'notice': 'ACTIVATED TO License Site Number: 391561492',
+            'product': 'SDSD23S45ZY',
+            'serial': 'SDSD23S45ZY',
+            'site_id': 'UNKNOWN',
+            'storage_capacity': '12457845',
+            'trial_license_ind': False,
+            'version': '1.0'}
+          ], 'license_text': '#######...'
+        }
         """
         return self.conn.get('license')
 
